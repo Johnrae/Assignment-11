94062ac7ea0f5637333bdd2db0ca279218f20d16
John Rae <john.rae23@gmail.com>
2015-10-08 16:54:46 -0400

    plenty o css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 (function () {
 
+  var string = $('#contentSpit').text();
+
+  var funStuff = _.template(string);
+
+
+  _.each(etsy.results, function(item){
+    var itemHTML = funStuff(item);
+  $('.content').append(itemHTML);
+  });
 
 
 }());
